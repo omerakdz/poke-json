be2c0389a89e112c05a1c1b9336101ac093a94c3
Ömer Akdeniz <s149155@ap.be>
2025-02-22 16:38:01 +0000

    wijzigingen

--- milestone1/index.ts
+++ milestone1/index.ts
@@ -9,40 +9,53 @@ let choices : string[] = [
     "Exit"
 ];
 
+let running : boolean = true;
+
 console.log("Welcome to the JSON data viewer!");
 
-let running : boolean = true;
+function showAllPokemon() {
+    for (const pokemon of pokeData.pokemon) {
+        console.log(`- ${pokemon.name} (${pokemon.id})`);
+    }
+}
+
+function filterById() {
+    let choiceId : number = readline.questionInt("Please enter the ID you want to filter by: ");
+    let correctId: boolean = false;
+
+    for (const pokemon of pokeData.pokemon) {
+        if (choiceId === pokemon.id) {
+            correctId = true;
+            console.log(`- ${pokemon.name} (${pokemon.id})`);
+            console.log(`Description: ${pokemon.description}`);
+            console.log(`Type: ${pokemon.type}`);
+            console.log(`Level: ${pokemon.level}`);
+            console.log(`Trainer: ${pokemon.trainer.name}`);
+        }
+    }
+    
+    if (!correctId) {
+        console.log("Pokémon ID niet gevonden.");
+    }
+}
+
 while(running){
     let input : number = readline.keyInSelect(choices, "Please enter your choice: ",{cancel: false}) ;
 
-    if (input === 0) {
-        for (let i = 0; i < pokeData.pokemon.length; i++) {
-            const pokemon: Pokemon = pokeData.pokemon[i];
-            console.log(`- ${pokemon.name} (${pokemon.id})`);
-        }
-    } 
-    else if(input === 1){
-        let choiceId : number = readline.questionInt("Please enter the ID you want to filter by: ");
-        let correctId : boolean = false;
-        for (let i = 0; i < pokeData.pokemon.length; i++) {
-            const pokemon: Pokemon = pokeData.pokemon[i];
+    switch (input) {
+        case 0:
+            showAllPokemon();
+            break;
+        case 1:
+            filterById();
+            break;
+        case 2:
+            running = false;
+            break;
+        default:
+            console.log("Ongeldige keuze.");
+    }
 
-            if (choiceId === pokemon.id) {
-                correctId = true;
-                console.log(`\n- ${pokemon.name} (${pokemon.id})`);
-                console.log(`Description: ${pokemon.description}`);
-                console.log(`Type: ${pokemon.type}`);
-                console.log(`Level: ${pokemon.level}`);
-                console.log(`Trainer: ${pokemon.trainer.name}`);
-            } 
-        }
-        if (!correctId) {
-            console.log("Pokémon ID niet gevonden.");
-        }
-    }
-    else if(input === 2){
-        running = false;
-    }
 }
 
 export {}
